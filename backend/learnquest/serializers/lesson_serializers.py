65fdc93533b664cf17d05a05b30a4da3b33fd77e
David Eje Oheji <ejeohejidavid@gmail.com>
2023-07-09 22:50:48 +0300

    Done with serializers for sections

---
+++ backend/learnquest/serializers/lesson_serializers.py
@@ -0,0 +1,21 @@
+from serializers_import import *
+
+class EnrolledViewLessonSerializer(serializers.ModelSerializer):
+    duration = serializers.SerializerMethodField(read_only = True)
+    class Meta:
+        model = Lesson
+        fields = ["title", "duration", "section", "type", "pdf", "video_url", "created_at", "updated_at"]
+        
+    def get_duration(self, instance):
+        return instance.duration
+    
+
+
+class UnenrolledViewLessonSerializer(serializers.Serializer):
+    duration = serializers.SerializerMethodField(read_only= True)
+    class Meta:
+        model = Lesson
+        fields = ['title',"section", "duration"]
+    
+    def get_duration(self, instance):
+        return instance.duration
---
+++ backend/learnquest/serializers/section_serializers.py
@@ -0,0 +1,45 @@
+from serializers_import import *
+from lesson_serializers import EnrolledViewLessonSerializer, UnenrolledViewLessonSerializer
+class RequestCreateSectionSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    course_id = serializers.UUIDField()
+    
+class ResponseCreateSectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Section
+        fields = "__all__"
+        
+class RequestUpdateSectionSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    
+class ResponseUpdateSectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Section
+        fields = "__all__"
+        
+class EnrolledViewSectionSerializer(serializers.ModelSerializer):
+    lessons = serializers.SerializerMethodField(read_only = True)
+    duration = serializers.SerializerMethodField(read_only = True)
+    class Meta:
+        model = Section
+        fields = ["duration", "lessons", "course", "title", "created_at", "updated_at"]
+        
+    def get_lessons(self,instance):
+        lessons = instance.lessons
+        return EnrolledViewLessonSerializer(instance=lessons, many = True).data
+        
+
+
+class UnenrolledViewSectionSerializer(serializers.Serializer):
+    lessons = serializers.SerializerMethodField(read_only = True)
+    duration = serializers.SerializerMethodField(read_only = True)
+    class Meta:
+        model = Section
+        fields = ["duration", "lessons", "course", "title", "created_at", "updated_at"]
+        
+    def get_lessons(self,instance):
+        lessons = instance.lessons
+        return UnenrolledViewLessonSerializer(instance=lessons, many = True).data
+
+
+        
