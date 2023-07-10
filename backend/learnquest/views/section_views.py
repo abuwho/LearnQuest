9750acce5282bde85c65ad714d2a2016d7eb928e
Abu Huraira <dear.huraira@gmail.com>
2023-07-10 06:22:18 +0300

    Update section_views.py

--- backend/learnquest/views/section_views.py
+++ backend/learnquest/views/section_views.py
@@ -44,6 +44,7 @@ def update_section(request):
 
         user = request.user
         title = serialized.data.get("title")
+
         section = serialized.data.get("section")
 
         section_object = Section.objects.get(id=section)
@@ -51,8 +52,9 @@ def update_section(request):
         if user != section_object.course.instructor:
             raise ValueError("The current user cannot update the section title")
 
-        section_object.title = title
-        section_object.save()
+        if title is not None:    
+            section_object.title = title
+            section_object.save()
 
         return Response(ResponseUpdateSectionSerializer(section_object).data, status=200)
     except Exception as e:
