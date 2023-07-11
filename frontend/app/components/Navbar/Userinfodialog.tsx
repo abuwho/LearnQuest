1460282e2453f55792813bb2bd58a0d4458ddd36
LaithAlebrahim <l.alebrahim@innopolis.university>
2023-07-12 02:02:09 +0300

    style username bug fixed

--- frontend/app/components/Navbar/Userinfodialog.tsx
+++ frontend/app/components/Navbar/Userinfodialog.tsx
@@ -4,7 +4,7 @@ import { LockClosedIcon } from '@heroicons/react/20/solid'
 import { UserIcon } from '@heroicons/react/24/outline'
 import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/solid'    
 import Link from 'next/link'
-
+import './Userinfodialog.css'
 
 const Userinfodialog = () => {
     let [isOpen, setIsOpen] = useState(false)
@@ -31,12 +31,12 @@ const Userinfodialog = () => {
     return (
         <>
             <div className="absolute inset-y-0 right-0 flex items-center sm:static sm:inset-auto sm:pr-0">
-                <div className='hidden lg:block'>
+                <div className='hidden lg:block icon-container '>
                     <button type="button" className='text-lg text-Blueviolet font-medium' onClick={openModal}>
                         {/* insert a user icon here */}
-                        <UserIcon>
+                        <UserIcon className='icon-image'>
                         </UserIcon>
-                        {username}
+                        <span className='username-span'>{username}</span>
                     </button>
                 </div>
             </div>
