3e9c1cd40a776811129f873fcdb98bcdddae3d21
Abu Huraira <dear.huraira@gmail.com>
2023-07-13 16:00:49 +0300

    Fix links on Footer and Navbar

--- frontend/app/components/Navbar/Navbar.tsx
+++ frontend/app/components/Navbar/Navbar.tsx
@@ -62,16 +62,18 @@ const Navbar = () => {
                             {/* LOGO */}
 
                             <div className="flex flex-shrink-0 items-center">
-                                <img
-                                    className="block h-12 w-40 lg:hidden"
-                                    src={'/assets/logo/logo.svg'}
-                                    alt="dsign-logo"
-                                />
-                                <img
-                                    className="hidden h-full w-full lg:block"
-                                    src={'/assets/logo/logo.svg'}
-                                    alt="dsign-logo"
-                                />
+                                <a href='/'>
+                                    <img
+                                        className="block h-12 w-40 lg:hidden"
+                                        src={'/assets/logo/logo.svg'}
+                                        alt="dsign-logo"
+                                    />
+                                    <img
+                                        className="hidden h-full w-full lg:block"
+                                        src={'/assets/logo/logo.svg'}
+                                        alt="dsign-logo"
+                                    />
+                                </a>
                             </div>
 
                             {/* LINKS */}
